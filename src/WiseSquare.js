dc58b7c96fc31a367aca362ca36de247596674ab
xaviguasch <xaviguasch@gmail.com>
2019-05-02 22:08:12 +0200

    added constructor to WiseSquare and bounded dispenseWisdom

--- src/WiseSquare.js
+++ src/WiseSquare.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react'
 import './WiseSquare.css'
 
 class WiseSquare extends Component {
-  dispenseWisdom() {
-    let messages = [
-      /* wise messages go here */
+  static defaultProps = {
+    messages: [
       'A fool thinks himself to be wise, but a wise man knows himself to be a fool.',
       'Educating the mind without educating the heart is no education at all.',
       'Not everything that is faced can be changed, but nothing can be changed until it is faced.'
     ]
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.dispenseWisdom = this.dispenseWisdom.bind(this)
+  }
+
+  dispenseWisdom() {
+    let { messages } = this.props
+
     let rIndex = Math.floor(Math.random() * messages.length)
     console.log(messages[rIndex])
   }
