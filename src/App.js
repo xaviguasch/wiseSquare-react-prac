91cfca6aabd45690e3f3acfb109f2745ea6880af
xaviguasch <xaviguasch@gmail.com>
2019-05-02 20:50:36 +0200

    added starter files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import WiseSquare from './WiseSquare'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>App Component</h1>
+      <WiseSquare />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
